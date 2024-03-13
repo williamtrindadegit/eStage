cfc0ff155ab220c7bc707ba98fb7743ed563a50c
Guillaume Legare <g23leg@gmail.com>
2024-03-13 16:25:29 -0400

    chips component

--- eStage/src/router/index.js
+++ eStage/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import TableauDeBord from "../views/Dashboard.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+    },
+    {
+      path: "/tableau-de-bord",
+      name: "tableau-de-bord",
+      component: TableauDeBord,
     },
   ],
 });
---
+++ eStage/src/views/Dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+  
+  <Chip />
+</template>
+
+<script>
+  import Chip from '@/components/Chip.vue'
+
+  export default {
+    components: {
+      Chip,
+    },
+  };
+</script>
+
+<style>
+
+</style>
