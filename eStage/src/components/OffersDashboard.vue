6d35bd080ffc54deaddc608b49843234e853d1df
Guillaume Legare <g23leg@gmail.com>
2024-03-18 17:03:53 -0400

    responsive dashboard

--- eStage/src/components/OffersDashboard.vue
+++ eStage/src/components/OffersDashboard.vue
@@ -1,15 +1,15 @@
 <template>
   <div className="bg-blue-100 mx-2 my-16 p-8 rounded-lg">
-    <div className=" flex justify-between">
+    <div className="grid grid-cols-1 md:flex justify-between">
       <div>
         <h2 className="text-black font-semibold">Dernière <span className="text-red-600">offres</span> de stage</h2>
         <p className="text-red-500">En attente de validation</p>
       </div>
-      <button className="btn bg-teal-500 border-teal-500 text-white">Valider toutes les offres</button>
+      <button className="btn mt-4 md:mt-0 bg-teal-500 border-teal-500 text-white">Valider toutes les offres</button>
     </div>
 
     <!-- Grid header -->
-    <div className="grid mt-20 grid-cols-4 gap-4">
+    <div className="hidden md:grid mt-20 grid-cols-4 gap-4">
       <div>Profil</div>
       <div>Ville</div>
       <div>Date</div>
@@ -17,12 +17,12 @@
     </div>
 
     <!-- Grid -->
-    <div className="grid mt-6 grid-cols-4 gap-4" v-for="demande in demandes">
+    <div className="mt-6 md:grid md:grid-cols-4 gap-4" v-for="demande in demandes">
       <div className="flex">
-        <div className="bg-red-600 p-3 w-12 rounded-lg">
+        <div className="hidden md:block bg-red-600 p-3 w-12 rounded-lg">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#ffffff" d="M32 32H480c17.7 0 32 14.3 32 32V96c0 17.7-14.3 32-32 32H32C14.3 128 0 113.7 0 96V64C0 46.3 14.3 32 32 32zm0 128H480V416c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V160zm128 80c0 8.8 7.2 16 16 16H336c8.8 0 16-7.2 16-16s-7.2-16-16-16H176c-8.8 0-16 7.2-16 16z"/></svg>
         </div>
-        <div className="text-black text-xs ml-2 my-auto">
+        <div className="text-black text-xs md:ml-2 my-auto">
           <h3 className="font-semibold">{{ demande.jobTitle }}</h3>
           <h4>{{ demande.nom }}</h4>
         </div>
@@ -30,7 +30,10 @@
       <div className="text-black text-xs flex items-center">{{ demande.ville }}</div>
       <div className="text-black text-xs flex items-center">{{ demande.date }}</div>
       <div className="text-xs flex justify-between items-center">
-        <button className="btn bg-emerald-200 border-emerald-200 text-emerald-600 font-light">Accepter</button>
+        <button className="w-6 md:w-8 lg:hidden xl:hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#059650" d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>
+        </button>
+        <button className="hidden lg:block btn bg-emerald-200 border-emerald-200 text-emerald-600 font-light">Accepter</button>
         <button className="w-6">
           <svg viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg"><path fill="rgb(20 184 166)" d="M572.52 241.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 32.35 0 0 0 0-29.19zM288 400a144 144 0 1 1 144-144 143.93 143.93 0 0 1-144 144zm0-240a95.31 95.31 0 0 0-25.31 3.79 47.85 47.85 0 0 1-66.9 66.9A95.78 95.78 0 1 0 288 160z"/></svg>
         </button>
@@ -41,6 +44,7 @@
           <svg enable-background="new 0 0 32 32" id="Glyph" version="1.1" viewBox="0 0 32 32" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path fill="rgb(220 38 38)" d="M6,12v15c0,1.654,1.346,3,3,3h14c1.654,0,3-1.346,3-3V12H6z M12,25c0,0.552-0.448,1-1,1s-1-0.448-1-1v-9  c0-0.552,0.448-1,1-1s1,0.448,1,1V25z M17,25c0,0.552-0.448,1-1,1s-1-0.448-1-1v-9c0-0.552,0.448-1,1-1s1,0.448,1,1V25z M22,25  c0,0.552-0.448,1-1,1s-1-0.448-1-1v-9c0-0.552,0.448-1,1-1s1,0.448,1,1V25z" id="XMLID_237_"/><path fill="rgb(220 38 38)" d="M27,6h-6V5c0-1.654-1.346-3-3-3h-4c-1.654,0-3,1.346-3,3v1H5C3.897,6,3,6.897,3,8v1c0,0.552,0.448,1,1,1h24  c0.552,0,1-0.448,1-1V8C29,6.897,28.103,6,27,6z M13,5c0-0.551,0.449-1,1-1h4c0.551,0,1,0.449,1,1v1h-6V5z" id="XMLID_243_"/></svg>
         </button>
       </div>
+      <hr className="w-full bg-slate-300 h-1">
     </div>
   </div>
 </template>
