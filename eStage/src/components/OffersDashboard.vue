3f70abb7cb23a346f3fc533a5186ea423b7f9839
Guillaume Legare <g23leg@gmail.com>
2024-03-18 19:49:10 -0400

    modif dashboard

--- eStage/src/components/OffersDashboard.vue
+++ eStage/src/components/OffersDashboard.vue
@@ -19,6 +19,7 @@
     <!-- Grid -->
     <div className="mt-6 md:grid md:grid-cols-4 gap-4" v-for="demande in demandes">
       <div className="flex">
+        <div class="bg-red-600 w-1 h-12 mr-2 md:mr-6"></div>
         <div className="hidden md:block bg-red-600 p-3 w-12 rounded-lg">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#ffffff" d="M32 32H480c17.7 0 32 14.3 32 32V96c0 17.7-14.3 32-32 32H32C14.3 128 0 113.7 0 96V64C0 46.3 14.3 32 32 32zm0 128H480V416c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V160zm128 80c0 8.8 7.2 16 16 16H336c8.8 0 16-7.2 16-16s-7.2-16-16-16H176c-8.8 0-16 7.2-16 16z"/></svg>
         </div>
