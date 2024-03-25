0170fd97fabe8c3fcd1d0f62d954c402211ccecd
Abdelhamid Benallou <hiabeno2020@gmail.com>
2024-03-25 15:39:43 -0400

    connexion

--- eStage/src/router/index.js
+++ eStage/src/router/index.js
@@ -1,9 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import Connexion from "../views/Connexion.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: "/Connexion",
+      name: "Connexion",
+      component: Connexion,
+    },
     {
       path: "/",
       name: "home",
