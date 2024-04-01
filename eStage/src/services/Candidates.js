0dc6ed4375c5627a5d7f0ea37538883b694543a9
williamtrindadegit <132960077+williamtrindadegit@users.noreply.github.com>
2024-04-01 19:47:53 -0400

    Fix appel d'apis

--- eStage/src/services/Candidates.js
+++ eStage/src/services/Candidates.js
@@ -1,26 +1,30 @@
 const BASE_URL = "https://api-2.fly.dev/candidates";
-import makeRequest from "./api";
+import api from "./api";
 
 export default {
   async FindOne(id) {
-    return makeRequest(`${BASE_URL}/${id}`, "GET");
+    return api.makeRequest(`${BASE_URL}/${id}`, "GET");
   },
   async FindAll() {
-    return makeRequest(BASE_URL, "GET");
+    return api.makeRequest(BASE_URL, "GET");
   },
   async Create(candidate) {
     const { post, ...rest } = candidate;
     const newSkills = [post, ...candidate.skills];
     const bodyContent = JSON.stringify({ ...rest, skills: newSkills });
-    return makeRequest(BASE_URL, "POST", JSON.parse(bodyContent));
+    return api.makeRequest(BASE_URL, "POST", JSON.parse(bodyContent));
   },
   async Update(candidate) {
     const { id, post, ...rest } = candidate;
     const newSkills = [post, ...candidate.skills];
     const bodyContent = JSON.stringify({ ...rest, skills: newSkills });
-    return makeRequest(`${BASE_URL}/${id}`, "PATCH", JSON.parse(bodyContent));
+    return api.makeRequest(
+      `${BASE_URL}/${id}`,
+      "PATCH",
+      JSON.parse(bodyContent)
+    );
   },
   async Delete(id) {
-    return makeRequest(`${BASE_URL}/${id}`, "DELETE");
+    return api.makeRequest(`${BASE_URL}/${id}`, "DELETE");
   },
 };
--- eStage/src/services/Provinces.js
+++ eStage/src/services/Provinces.js
@@ -1,20 +1,20 @@
 const BASE_URL = "https://api-2.fly.dev/provinces";
-import makeRequest from "./api";
+import api from "./api";
 
 export default {
   async FindOne(id) {
-    return makeRequest(`${BASE_URL}/${id}`, "GET");
+    return api.makeRequest(`${BASE_URL}/${id}`, "GET");
   },
   async FindAll() {
-    return makeRequest(BASE_URL, "GET");
+    return api.makeRequest(BASE_URL, "GET");
   },
   async Create(province) {
-    return makeRequest(BASE_URL, "POST", province);
+    return api.makeRequest(BASE_URL, "POST", province);
   },
   async Update(province) {
-    return makeRequest(BASE_URL, "PATCH", province);
+    return api.makeRequest(BASE_URL, "PATCH", province);
   },
   async Delete(id) {
-    return makeRequest(`${BASE_URL}/${id}`, "DELETE");
+    return api.makeRequest(`${BASE_URL}/${id}`, "DELETE");
   },
 };
