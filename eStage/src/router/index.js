0575134fa1d7d9bfa332576d6c777799d887a3ff
williamtrindadegit <132960077+williamtrindadegit@users.noreply.github.com>
2024-04-01 15:51:44 -0400

    Fix routes et remove page en doublon

--- eStage/src/router/index.js
+++ eStage/src/router/index.js
@@ -2,9 +2,9 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import Dashboard from "../views/Dashboard.vue";
 import ZoomUser from "../components/ZoomUser.vue";
+import ZoomStage from "../components/ZoomStage.vue";
 import InternshipDashboard from "@/components/InternshipDashboard.vue";
 import LoginPage from "@/components/LoginPage.vue";
-import Connexion from "../views/Connexion.vue";
 import FormCandidateView from "@/views/FormCandidateView.vue";
 import FormInternShipOfferView from "@/views/FormInternShipOfferView.vue";
 import FormInternShipRequestView from "@/views/FormInternShipRequestView.vue";
@@ -13,11 +13,6 @@ import FormEnterpriseView from "@/views/FormEnterpriseView.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: "/Connexion",
-      name: "Connexion",
-      component: Connexion,
-    },
     {
       path: "/",
       name: "home",
@@ -32,6 +27,11 @@ const router = createRouter({
       path: "/zoomUser",
       name: "zoomUser",
       component: ZoomUser,
+    },
+    {
+      path: "/zoomStage",
+      name: "zoomStage",
+      component: ZoomStage,
     },
     {
       path: "/InternshipDashboard",
