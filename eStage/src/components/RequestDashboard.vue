a98ffa9bf6f41a33e527f5b76f1c6a93b931e1f7
Guillaume Legare <g23leg@gmail.com>
2024-04-04 15:09:06 -0400

    test

--- eStage/src/components/RequestDashboard.vue
+++ eStage/src/components/RequestDashboard.vue
@@ -46,8 +46,9 @@
         </button>
         <button @click="accepterDemande(demande)" className="hidden lg:block btn bg-emerald-200 border-emerald-200 text-emerald-600 font-light">Accepter</button>
         <button className="w-6">
-          <router-link to="/zoomUser/{{ demande.id }}"></router-link>
-          <svg viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg"><path fill="rgb(20 184 166)" d="M572.52 241.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 32.35 0 0 0 0-29.19zM288 400a144 144 0 1 1 144-144 143.93 143.93 0 0 1-144 144zm0-240a95.31 95.31 0 0 0-25.31 3.79 47.85 47.85 0 0 1-66.9 66.9A95.78 95.78 0 1 0 288 160z"/></svg>
+          <router-link to="/zoomUser/{{ demande.id }}">
+            <svg viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg"><path fill="rgb(20 184 166)" d="M572.52 241.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 32.35 0 0 0 0-29.19zM288 400a144 144 0 1 1 144-144 143.93 143.93 0 0 1-144 144zm0-240a95.31 95.31 0 0 0-25.31 3.79 47.85 47.85 0 0 1-66.9 66.9A95.78 95.78 0 1 0 288 160z"/></svg>
+          </router-link>
         </button>
         <button className="w-6">
           <router-link to="/editinternshiprequest/{{demande.id}}">
